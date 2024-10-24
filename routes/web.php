9338bc0f561bbd11d555cae2d7277c62a55da01f
hamdanmaulanamuhammad <160691851+hamdanmaulanamuhammad@users.noreply.github.com>
2024-10-24 15:58:36 +0700

    login-reset done

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//FE
+Route::get('/login-fe', function(){
+    return view('login-fe');
+})->name('login-fe');
+
+Route::get('/forgot-password-fe', function(){
+    return view('forgot-password-fe');
+})->name('forgot-password-fe');
+
+Route::get('/verification-otp-fe', function(){
+    return view('verification-otp-fe');
+})->name('verification-otp-fe');
+
+Route::get('/reset-password-fe', function(){
+    return view('reset-password-fe');
+})->name('reset-password-fe');
