645292571b281bbe6dc193fcf98de9df3c24da4d
djatiaja <djamgt23@gmail.com>
2024-12-02 15:33:00 +0700

    adding seed notification to admin

--- database/seeders/AdminSeed.php
+++ database/seeders/AdminSeed.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\notification;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
@@ -23,5 +24,16 @@ class AdminSeed extends Seeder
             "role_id" =>1,
             'remember_token' => Str::random(10),
         ]);
+            
+        $admins = User::where('role_id', 1)->get();
+
+        foreach ($admins as $admin) {
+            notification::factory()->create([
+                'user_id' => $admin->id,
+                'title' => 'Ruang Jaya Print',
+                'message' => 'Halo Admin! saya baru saja bergabung di SAMIKADOS'
+            ]);
+        }
     }
+
 }
