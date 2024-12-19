d24315dd386da76c02bcaca97f25f0fd3b15af89
djatiaja <djamgt23@gmail.com>
2024-12-19 18:53:47 +0700

    fix https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,8 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->environment(env("APP_ENV"))) {
-            URL::forceScheme('https');
+        if ($this->app->environment('production')) {
+            \URL::forceScheme('https');
         }
 
         Schema::defaultStringLength(150 );
