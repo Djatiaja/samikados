3f406f7424c6904f4b6336d7572916a5de56e687
djatiaja <djamgt23@gmail.com>
2024-12-19 16:56:04 +0700

    setup heroku

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -28,7 +28,7 @@ return new class extends Migration
             $table->string("photo")->nullable()->default('https://placehold.co/600x400');
             $table->string("provider")->nullable();
             $table->string("provider_id")->nullable();
-            $table->string("provider_token")->nullable();
+            $table->string("provider_token", 1500)->nullable();
             $table->boolean("is_suspended")->nullable();
             $table->rememberToken();
             $table->timestamps();
