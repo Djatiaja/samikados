99a7df8afe334dc3b4aee7fc84f69856e24d9620
djatiaja <djamgt23@gmail.com>
2024-12-19 14:35:11 +0700

    setup heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Schema::defaultStringLength(255);
+        Schema::defaultStringLength(500);
 
 
         VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
